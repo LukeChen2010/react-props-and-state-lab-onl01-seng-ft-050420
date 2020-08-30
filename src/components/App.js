0fe067dae40fa948525b90d1a90a8cac9bcff8dc
Luke Chen <luke.chen.2010@gmail.com>
2020-08-30 19:11:00 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
               />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
